659c932003365987f7c45e6e804703658cc7508d
Saúl Hormazábal <saul.hormazabal@gmail.com>
2019-03-06 01:35:09 -0300

    Fixed signup form for use email field

--- users/views.py
+++ users/views.py
@@ -1,13 +1,14 @@
 from django.contrib.auth import authenticate
 from django.contrib.auth import get_user_model
 from django.contrib.auth import login
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import redirect
 from django.shortcuts import render
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView
 from django.views.generic.edit import UpdateView
+
+from .forms import UserCreationForm
 
 
 User = get_user_model()
@@ -19,14 +20,14 @@ def signup(request):
     if form.is_valid():
         form.save()
 
-        username = form.cleaned_data.get('username')
+        email = form.cleaned_data.get('email')
         raw_password = form.cleaned_data.get('password1')
 
-        user = authenticate(username=username, password=raw_password)
+        user = authenticate(email=email, password=raw_password)
 
         login(request, user)
 
-        return redirect('index')
+        return redirect('classroom-create')
 
     return render(request, 'users/signup.html', {'form': form})
 
