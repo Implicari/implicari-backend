f8ee8425334732231ee7adf9ea6d8c6c49a89fca
Saúl Hormazábal <saul.hormazabal@gmail.com>
2019-06-01 20:36:31 -0400

    Test of creation an event

--- events/tests.py
+++ events/tests.py
@@ -2,6 +2,7 @@ from django.contrib.auth import get_user_model
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from django.test import Client
 
+from classrooms.models import Classroom
 from events.models import Event
 
 from .tasks import send_email_event
@@ -47,6 +48,32 @@ class EventCreateViewTestCase(StaticLiveServerTestCase):
         response = client.get('/cursos/1/eventos/crear/', secure=True)
 
         self.assertEqual(response.status_code, 200)
+
+    def test_post(self):
+        user = User.objects.get(email='saul.hormazabal@gmail.com')
+
+        client = Client()
+        client.force_login(user)
+
+        classroom = Classroom.objects.first()
+
+        self.assertFalse(Event.objects.exists())
+
+        data = {
+            'description': 'Lorem',
+            'message': 'Ipsum',
+            'date': '2019-06-01',
+        }
+
+        response = client.post(f'/cursos/{classroom.id}/eventos/crear/', data, secure=True)
+
+        self.assertEqual(Event.objects.count(), 1)
+
+        self.assertRedirects(
+            response,
+            f'/cursos/{classroom.id}/eventos/',
+            fetch_redirect_response=False,
+        )
 
 
 class EventDetailViewTestCase(StaticLiveServerTestCase):
